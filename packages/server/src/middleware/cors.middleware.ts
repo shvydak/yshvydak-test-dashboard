78af8a259d8f0e9f58b81db3699bb2a8e653818b
Yurii Shvydak <35421423+shvydak@users.noreply.github.com>
2025-10-22 12:39:23 +0300

    Change CORS credentials option to false

--- packages/server/src/middleware/cors.middleware.ts
+++ packages/server/src/middleware/cors.middleware.ts
@@ -2,7 +2,7 @@ import cors from 'cors'
 
 export const corsOptions = {
     origin: true, // Allow all origins for development
-    credentials: true,
+    credentials: false,
     optionsSuccessStatus: 200,
 }
 
